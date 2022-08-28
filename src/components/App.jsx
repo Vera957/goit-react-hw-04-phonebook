ae4b6a5a3e158cd0a12146e0c21d8cea6f45d016
Vera957 <good.answer.e@gmail.com>
2022-08-28 23:31:34 +0300

    hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,81 +1,60 @@
 import React from "react";
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import { ContactList } from "./ContactList";
 import { Box } from "./Box";
 import { FormikContactForm } from "./FormikContactForm/FormikContactForm";
 import { GlobalStyle } from "GlobalStyle";
 import { Field2 } from './Filter2/Filter2';
 
-const contacts = 'contacts';
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: ''
+  useEffect(() => {
+    if (localStorage.getItem('contacts') !== null) {
+      const savedContacts = localStorage.getItem('contacts');
+      const parsedsavedContacts = JSON.parse(savedContacts);
+      setContacts(parsedsavedContacts);
+    }
+  }, []);
+
+   const formSubmitData = (data) => {
+    const noPass = contacts.filter(item => item.name.toLowerCase() === data.name.toLowerCase());
+    if (noPass.length < 1) {
+      setContacts([...contacts, data]);
+      const savedContacts = JSON.stringify(contacts)
+      localStorage.setItem('contacts', savedContacts)
+    } else alert(`${data.name} alredy in contacts.`);
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if(prevState.contacts.length !== this.state.contacts.length){
-    const savedContacts = JSON.stringify(this.state.contacts)
-    localStorage.setItem(contacts, savedContacts)}
-  }
+  //const filterContactsName = values => setFilter(values.filter);
 
-  componentDidMount() {
-    if(localStorage.getItem(contacts) !== null){
-    const savedContacts = localStorage.getItem(contacts);
-    const parsedsavedContacts = JSON.parse(savedContacts)
-    this.setState({ contacts: parsedsavedContacts })}
-  }
-
-  formSubmitData = (data) => {
-    const noPass = this.state.contacts.filter(item => item.name.toLowerCase() === data.name.toLowerCase());
-    noPass.length < 1 ?
-      this.setState(prevState => ({
-        contacts: [...prevState.contacts, data]
-      })) : alert(`${data.name} alredy in contacts.`);
-  }
-
-  filterContactsName = (values) => {
-    this.setState({
-      filter: values.filter,
-    })
-  }
-
-  deleteContact = (id) => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(item => item.id !== id),
-    }))
-  }
-
-   filteredList = contacts =>  this.state.filter === '' ?
-      contacts :
-    contacts.filter(item => item.name.includes(this.state.filter));
+  const deleteContact = id => setContacts(prevState => { prevState.contacts.filter(item => item.id !== id) })
   
-  show = (data) => this.setState({filter: data})
-  
-  render() {
-    const { contacts } = this.state;
-    return (   
-      <Box
-        m={4}
-        p={4}
-        height="100vh"
-        bg='#f6f6f6'
-        maxWidth='500px'
-        ml='auto'
-        mr='auto'
-      ><GlobalStyle />
-        <FormikContactForm onNewVal={this.formSubmitData} />
-        <ContactList
-          data={this.filteredList(contacts)}
-          deleteContact={this.deleteContact}>
-          {/* <Filter filterContactsName={this.filterContactsName} />*/}
-          <Field2 onChange={this.show} />
-        </ContactList>
-        </Box>
-      )
-  }
+  const filteredList = contacts => filter === '' ?
+    contacts :
+    contacts.filter(item => item.name.includes(filter));
+
+  const show = (data) => setFilter(data);
+
+  return (<>
+    <Box
+      m={4}
+      p={4}
+      height="100vh"
+      bg='#f6f6f6'
+      maxWidth='500px'
+      ml='auto'
+      mr='auto'
+    ><GlobalStyle />
+      <FormikContactForm onNewVal={formSubmitData} />
+      <ContactList
+        data={filteredList(contacts)}
+        deleteContact={deleteContact}>
+        {/* <Filter filterContactsName={this.filterContactsName} />*/}
+        <Field2 onChange={show} />
+      </ContactList>
+    </Box>
+  </>
+  )
 }
-
-
-
